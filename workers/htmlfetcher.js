37d96a4ed903da853e968918eec61c8aa6d65276
Hack Reactor Students <github@hackreactor.com>
2013-08-28 15:00:09 -0700

    Working on htmlfetcher.js

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,23 @@
-// eventually, you'll have some code here that uses the tested helpers 
+// eventually, you'll have some code here that uses the tested helpers
 // to actually download the urls you want to download.
+
+//command line: node htmlfetcher.js /path/to/file.txt
+//process.argv[0] = node;
+//process.argv[1] = htmlfetcher.js;
+//process.argv[2] = /path/to/file.txt;
+
+
+var fetcherHelpers = require("./lib/html-fetcher-helpers.js");
+
+if (process.argv.length < 3) {
+  console.log('Usage: node ' + process.argv[1] + ' FILEPATH');
+  process.exit(1);
+} else {
+//get the filepath from the command line
+var filePath = process.argv[2];
+
+fetcherHelpers.readUrls(filePath);
+
+}
+
+
--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,7 +1,21 @@
-exports.readUrls = function(filePath, cb){
+var fs = require('fs');
 
+exports.readUrls = function(filePath, cb){
+  //loop through each line in sites.txt
+  //and pass each url in the downloadUrls function
+
+  //given the filepath, take the file and read it line by line.
+  fs.readFile(filePath, 'utf8', function(err, data) {
+    if (err) {
+      console.log("ERROR: " + err);
+    } else {
+      console.log('OK: ' + filePath);
+      console.log(data);
+    }
+  });
 };
 
 exports.downloadUrls = function(urls){
-  // fixme
+  //Takes a given url and downloads the page at the current time, the function
+  //is called, if the file exists then overwrite it.
 };
